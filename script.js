06319bddf3aab373691fa10b3008104ed63fbd28
HeitorBarbosaDaRocha <134523231+HeitorBarbosaDaRocha@users.noreply.github.com>
2023-11-10 12:10:17 -0300

    a

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function mudarTexto() {
     document.getElementById("demo").innerHTML = "Ataque do Funk Kong!";
 }
+    document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
